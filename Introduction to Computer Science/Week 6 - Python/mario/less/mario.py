fec6a508e78efd1084abbcfaa374ed190ff627c9
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-09-24 14:26:56 +0630

    Python

---
+++ Introduction to Computer Science/Week 6 - Python/mario/less/mario.py
@@ -0,0 +1,10 @@
+from cs50 import get_int
+
+height = get_int("Height: ")
+
+while height <= 0 or height > 8:
+    height = get_int("Height: ")
+
+for i in range(1, height + 1):
+    print(" " * (height - i), end="")
+    print("#" * i)
