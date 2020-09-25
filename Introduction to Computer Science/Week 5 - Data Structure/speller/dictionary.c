66afdb2fb2bd1a2201fd0076d28def7be9f1f299
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-09-25 16:15:17 +0630

    Speller Finished

--- Introduction to Computer Science/Week 5 - Data Structure/speller/dictionary.c
+++ Introduction to Computer Science/Week 5 - Data Structure/speller/dictionary.c
@@ -2,14 +2,13 @@
 
 #include <stdbool.h>
 #include <stdio.h>
+
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <strings.h>
 
-
 #include "dictionary.h"
-
 // Represents a node in a hash table
 typedef struct node
 {
@@ -20,29 +19,29 @@ node;
 
 // Number of buckets in hash table
 const unsigned int N = 26;
-// total words
-unsigned int totalWords = 0;
+unsigned int word_count = 0;
 // Hash table
 node *table[N];
 
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
+    // TODO
+    int index = hash(word);
 
-    // get node value
-    node *cursor = table[hash(word)];
+    node *n = table[index];
 
-    // loop until no node
-    while (cursor != NULL)
+    if (n == NULL)
     {
-        // compare word and node word case-insensitve
-        if (strcasecmp(word, cursor->word) == 0)
+        return false;
+    }
+
+    for (node *temp = n; temp != NULL; temp = temp->next)
+    {
+        if (strcasecmp(word, temp->word) == 0)
         {
             return true;
         }
-
-        // change to next node
-        cursor = cursor->next;
     }
 
     return false;
@@ -51,100 +50,75 @@ bool check(const char *word)
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    // this is from stackoverflow
-    unsigned long hash = 5381;
-    int c;
+    // TODO
 
-    while ((c = tolower(*word++)))
+    // hashing function is first letter of words
+    int index = 0;
+    int i = tolower(word[0]);
+
+    if (i >= 97 && i <= 122)
     {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+        index = i - 97;
+
     }
 
-
-    return hash % N;
+    return index;
 }
 
 // Loads dictionary into memory, returning true if successful else false
 bool load(const char *dictionary)
 {
-    // open file
-    FILE *inputFile = fopen(dictionary, "r");
+    // TODO
+    FILE *f = fopen(dictionary, "r");
 
-    // return false if file not exist
-    if (inputFile == NULL)
+    if (f == NULL)
     {
         return false;
     }
 
-    // declare for each word
-    char word[LENGTH + 1];
-
-    // loop for reading file until the end which is EOF
-    while (fscanf(inputFile, "%s", word) != EOF)
+    char word[LENGTH];
+    while (fscanf(f, "%s", word) != EOF)
     {
-        // initialize node
         node *n = malloc(sizeof(node));
 
-        // check node is valid
         if (n == NULL)
         {
             return false;
         }
 
-        // copy word to node
         strcpy(n->word, word);
 
-        // point to head with new node
-        n->next = table[hash(word)];
-
-        // store head with node
-        table[hash(word)] = n;
-
-        totalWords++;
+        int index = hash(n->word);
+        n->next = table[index];
+        table[index] = n;
+        word_count++;
     }
-
-    // close input file
-    fclose(inputFile);
-
-    // done loading file
+    fclose(f);
     return true;
 }
 
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
-    return totalWords;
+    // TODO
+    return word_count;
 }
 
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
-    // loop for hash table
+    // TODO
     for (int i = 0; i < N; i++)
     {
-        // get array of linkedlist
         node *cursor = table[i];
 
-        // loop until array is empty
-        while (cursor)
+        while (cursor != NULL)
         {
-            // temp for free
             node *temp = cursor;
-
-            // change cursor to next
             cursor = cursor->next;
-
-            // free memory
             free(temp);
+
         }
-
-        // check if table end and cursor is null
-        if (i == N - 1 && cursor == NULL)
-        {
-            return true;
-        }
-
-
     }
-    return false;
+    return true;
 }
