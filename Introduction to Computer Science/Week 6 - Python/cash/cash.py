15dc9887dfdbe520be2bd9101d44fbe967d8516b
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-09-25 18:30:55 +0630

    Cash Finished

--- Introduction to Computer Science/Week 6 - Python/cash/cash.py
+++ Introduction to Computer Science/Week 6 - Python/cash/cash.py
@@ -1,25 +1,25 @@
 from cs50 import get_float
 
-dollars = get_float("Change owed: ")
+cash = get_float("Changed owed: ")
 
-while dollars <= 0:
-    dollars = get_float("Change owed: ")
+while cash <= 0:
+    cash = get_float("Changed owed: ")
 
-cents = round(dollars * 100)
+cash = round(cash * 100)
+coins = 0
 
-numOfCoins = 0
-
-while cents > 0:
-    if cents >= 25:
-        cents -= 25
-        numOfCoins += 1
-    elif cents >= 10:
-        cents -= 10
-        numOfCoins += 1
-    elif cents >= 5:
-        cents -= 5
-        numOfCoins += 1
+while cash > 0:
+    if cash >= 25:
+        cash -= 25
+        coins += 1
+    elif cash >= 10:
+        cash -= 10
+        coins += 1
+    elif cash >= 5:
+        cash -= 5
+        coins += 1
     else:
-        cents -= 1
-        numOfCoins += 1
-print(numOfCoins)
+        cash -= 1
+        coins += 1
+
+print(f"{coins}")
