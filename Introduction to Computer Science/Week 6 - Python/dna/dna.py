864a2d5afb91c5ce6aad9073025a3d5bc2214c71
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-09-25 20:44:24 +0630

    DNA Finished

--- Introduction to Computer Science/Week 6 - Python/dna/dna.py
+++ Introduction to Computer Science/Week 6 - Python/dna/dna.py
@@ -1,91 +1,64 @@
+from csv import reader
 from sys import argv
-from csv import reader, DictReader
 
-# exit input is invalid
-if len(argv) < 3:
+
+# checking argument counts
+if (len(argv) != 3):
     print("Usage: python dna.py data.csv sequence.txt")
     exit(1)
-# read database file
-with open(argv[1], "r") as dataFile:
-    data = list(reader(dataFile))
-# read input file
-with open(argv[2], "r") as searchFile:
-    searchData = searchFile.read()
 
-# remove name
-data[0].remove("name")
-# get sequence
-sequences = data[0]
+people_dna = []
+# open csv file
+try:
+    with open(argv[1], "r") as database_file:
+        if (database_file == None):
+            print("Can't open file")
+            exit(1)
+        data = reader(database_file)
+        for row in data:
+            people_dna.append(row)
+except FileNotFoundError:
+    print("Could not open file!")
 
-sequencesCount = []
-# loop for each sequence
-for i in range(len(sequences)):
-    # setting sequence position
-    currPos = 0
-    # setting sequence count
+# open sequence file
+try:
+    with open(argv[2], "r") as sequence_file:
+        if (sequence_file == None):
+            print("Can't open file")
+            exit(1)
+        sequences = sequence_file.read()
+
+except FileNotFoundError:
+    print("Could not open file!")
+
+# search STR sequence
+dna = people_dna[0]
+# remove first row
+dna.pop(0)
+people_dna.pop(0)
+
+# find dna sequences from sequence file
+person_dna = []
+for i in dna:
+    j = i
     count = 0
-    # setting prev sequence position
-    prevPos = 0
-    # setting maximum count
-    maxCount = 0
-    # loop for searching sequence
-    while currPos < len(searchData):
-        # finding sequence position
-        currPos = searchData.find(sequences[i], currPos)
-        # no sequence found
-        if (currPos == -1):
-            count = 0
-            break
-        # sequence found
-        elif (currPos != -1 and prevPos == 0):
-            count += 1
-            prevPos = currPos
-            maxCount = count
-        # checking sequence is contiguous
-        elif (currPos != -1 and currPos - prevPos == len(sequences[i])):
-            count += 1
-            prevPos = currPos
-            if maxCount < count:
-                maxCount = count
-        # new sequence found
-        elif (currPos != 1 and currPos - prevPos != len(sequences[i])):
-            count = 1
-            prevPos = currPos
-            if maxCount < count:
-                maxCount = count
-        # incement position
-        currPos += 1
-    # save maxcount
-    sequencesCount.append(maxCount)
+    while (i in sequences):
+        count += 1
+        i += j
+    person_dna.append(f"{count}")
 
-# remove element 0 which is sequence
-data.pop(0)
-# initialze name
-name = False
-# sequence max count value
-value = []
-# loop for each row in data.csv
-for i in range(len(data)):
-    # get one row
-    values = data[i]
-    # clean value
-    value = []
-    # loop for rwo
-    for v in range(len(values)):
-        # skip name
-        if v == 0:
+# check dna sequneces in database file
+is_found = False
+for i in people_dna:
+    d = []
+    for j in range(len(i)):
+        if (j == 0):
             continue
-        # add value
-        value.append(int(values[v]))
-    # checking value is equal to sequencecount
-    if value == sequencesCount:
-        # set name
-        name = values[0]
-        # exit the loop
+        d.append(i[j])
+    if (person_dna == d):
+        print(i[0])
+        is_found = True
         break
-# print answer
-if name:
-    print(name)
-else:
-    print("No match")
 
+if (is_found == False):
+    print("No match")
